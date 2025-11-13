2add144ab7fe27f848282ffa550f569a5a0421ce
Zifan Si <siz@mcmaster.ca>
2025-11-13 15:02:38 -0500

    wrapped by the .container layout.

--- src/react-app/src/layouts/TwoByTwoGrid.js
+++ src/react-app/src/layouts/TwoByTwoGrid.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "../styles/TwoByTwoGrid.css";
 
 export default function TwoByTwoGrid({ topLeft, topRight, bottomLeft, bottomRight }) {
   return (
-    <div className="container">{/* 12-col grid from your CSS */}
-      <div className="grid-col-8">{topLeft}</div>
-      <div className="grid-col-4">{topRight}</div>
-      <div className="grid-col-8">{bottomLeft}</div>
-      <div className="grid-col-4">{bottomRight}</div>
+    <div className="twoByTwoGrid">
+      <div className="cell">{topLeft}</div>
+      <div className="cell">{topRight}</div>
+      <div className="cell">{bottomLeft}</div>
+      <div className="cell">{bottomRight}</div>
     </div>
   );
 }
