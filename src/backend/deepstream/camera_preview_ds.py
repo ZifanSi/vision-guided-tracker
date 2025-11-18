11396e0cec2134787661f0300870f93d2070732f
Pegasis <me@pegasis.site>
2025-11-18 17:24:02 -0500

    deepstream

--- src/backend/deepstream/camera_preview_ds.py
+++ src/backend/deepstream/camera_preview_ds.py
@@ -1,5 +1,6 @@
 import sys
-
+import os
+os.environ["GST_DEBUG_DUMP_DOT_DIR"] = "/tmp/"
 sys.path.append('../')
 import gi
 
@@ -44,20 +45,34 @@ def inference_stop_probe(pad, info, u_data):
     return Gst.PadProbeReturn.OK
 
 _fps_last_time = time.perf_counter()
+_fps_time_list = [0]
+_inspected = False
 
 def osd_sink_pad_buffer_probe(pad, info, u_data):
     global _fps_last_time
+    global _fps_time_list
+    global _inspected
+    global pipeline
+
+    # if not _inspected:
+    #     # inspect_caps(pipeline)
+    #     Gst.debug_bin_to_dot_file(pipeline, Gst.DebugGraphDetails.ALL, "pipeline")
+    #     _inspected = True
 
     now = time.perf_counter()
     elapsed = now - _fps_last_time
-    fps = 1 / elapsed
-    print(f"FPS: {fps:.2f}, frame time: {elapsed * 1000:.2f} ms")
+    avg_fps = len(_fps_time_list)/(now - _fps_time_list[0])
+    print(f"FPS: {avg_fps:.1f}, frame time: {elapsed * 1000:.1f} ms")
     _fps_last_time = now
+    _fps_time_list.append(now)
+    if len(_fps_time_list) > 60:
+        _fps_time_list.pop(0)
 
     return Gst.PadProbeReturn.OK
 
 
 def main():
+    global pipeline
     Gst.init(None)
 
     camera = "/dev/video0"
@@ -99,8 +114,9 @@ def main():
         sys.stderr.write(" Unable to create sink \n")
 
     source.set_property('device', camera)
+    source.set_property('cap-buffers', 2)
     # source.set_property('io-mode', 2)
-    caps_v4l2src.set_property('caps', Gst.Caps.from_string("framerate=60/1, width=1920, height=1080"))
+    caps_v4l2src.set_property('caps', Gst.Caps.from_string("framerate=60/1,width=1920,height=1080"))
     streammux.set_property('width', 1920)
     streammux.set_property('height', 1080)
     streammux.set_property('live-source', 1)
@@ -133,6 +149,16 @@ def main():
     bus = pipeline.get_bus()
     bus.add_signal_watch()
     bus.connect("message", bus_call, loop)
+    #
+    # for pad in source.pads:
+    #     caps = pad.get_current_caps()
+    #     if not caps:
+    #         caps = pad.get_allowed_caps()
+    #     if caps:
+    #         print(f"  Pad: {pad.get_name()}")
+    #         for i in range(caps.get_size()):
+    #             s = caps.get_structure(i)
+    #             print(f"    {s.to_string()}")
 
     sinkpad = sink.get_static_pad("sink")
     if not sinkpad:
@@ -140,14 +166,14 @@ def main():
 
     sinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
 
-    # infersinkpad = pgie.get_static_pad("sink")
-    # if not infersinkpad:
-    #     sys.stderr.write(" Unable to get sink pad of pgie \n")
-    # infersinkpad.add_probe(Gst.PadProbeType.BUFFER, inference_start_probe, 0)
-    # infersrcpad = pgie.get_static_pad("src")
-    # if not infersrcpad:
-    #     sys.stderr.write(" Unable to get src pad of pgie \n")
-    # infersrcpad.add_probe(Gst.PadProbeType.BUFFER, inference_stop_probe, 0)
+    infersinkpad = pgie.get_static_pad("sink")
+    if not infersinkpad:
+        sys.stderr.write(" Unable to get sink pad of pgie \n")
+    infersinkpad.add_probe(Gst.PadProbeType.BUFFER, inference_start_probe, 0)
+    infersrcpad = pgie.get_static_pad("src")
+    if not infersrcpad:
+        sys.stderr.write(" Unable to get src pad of pgie \n")
+    infersrcpad.add_probe(Gst.PadProbeType.BUFFER, inference_stop_probe, 0)
 
     print("Starting pipeline \n")
     pipeline.set_state(Gst.State.PLAYING)
@@ -158,14 +184,37 @@ def main():
     # cleanup
     pipeline.set_state(Gst.State.NULL)
 
+def inspect_caps(pipeline):
+    pipeline.set_state(Gst.State.PAUSED)
+    pipeline.get_state(Gst.CLOCK_TIME_NONE)
+
+    print("=== Pipeline negotiated caps ===")
+    iter = pipeline.iterate_elements()
+    elem = iter.next()
+    while elem:
+        elem = elem[1] if isinstance(elem, tuple) else elem
+        if not elem:
+            break
+        print(f"\nElement: {elem.get_name()}")
+        for pad in elem.pads:
+            caps = pad.get_current_caps()
+            if not caps:
+                caps = pad.get_allowed_caps()
+            if caps:
+                print(f"  Pad: {pad.get_name()}")
+                for i in range(caps.get_size()):
+                    s = caps.get_structure(i)
+                    print(f"    {s.to_string()}")
+
+        elem = iter.next()
 
 if __name__ == '__main__':
-    import os
-
     # sudo is configured to not require password
     os.system("sudo nvpmodel -m 2") # MAXN_SUPER mode
     os.system("sudo jetson_clocks") # set all clocks to max
     os.system("sudo modprobe nvidia-drm modeset=1") # I can't get this to persist across reboots
     os.nice(-10)
+
+
     # set_display_env()
     sys.exit(main())
